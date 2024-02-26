24972f083b6ab5db518c126a62730068709b3189
username <you@yourdomain.example.com>
2024-02-26 16:21:35 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,76 @@
+function Footer()
+{
+    return(<div className="bg-dark text-light">
+    <div class="container-fluid text-center">
+  <div class="row align-items-start">
+    <div class="col">
+    <hr class="border border-light border-1 opacity-50"></hr>
+    </div>
+    <div class="col">
+    <a className="text-decoration-none " href="#">
+            <p className="h4 text-light">
+              <span className="span">F</span>est
+              <span><i className="bi bi-lightning color span"></i></span>
+              <i className="fa-sharp fa-solid fa-u text-danger "></i>p
+            </p>
+          </a>
+    </div>
+    <div class="col">
+    <hr class="border border-light border-1 opacity-50"></hr>
+    </div>
+  </div>
+  <div class="card bg-transparent border border-0">
+ 
+  <div class="card-body">
+  <div class="d-flex flex-row mb-3 text-light justify-content-center ">
+  <div class="p-2 fs-1"> <i class="fa-brands fa-x-twitter"></i></div>
+  <div class="p-2 fs-1"><i class="fa-brands fa-facebook text-primary"></i></div>
+  <div class="p-2 fs-1"><i class="fa-brands fa-linkedin text-primary"></i></div>
+  <div class="p-2 fs-1"><i class="fa-brands fa-pinterest text-danger"></i></div>
+  <div class="p-2 fs-1"><i class="fa-brands fa-whatsapp text-success"></i></div>
+  <div class="p-2 fs-1"><i class="fa-brands fa-instagram"></i></div>
+</div>
+  </div>
+</div>
+<div class="card bg-transparent border border-0 ">
+ 
+  <div class="card-body ">
+
+
+
+  <div class="row">
+  <div class="col-sm-6 mb-sm-0 ">
+    <div class="card border border-0 bg-transparent">
+      <div class="card-body ">
+      <div class="d-flex flex-row justify-content-around">
+<div class="px-2"><p><a href="#" class="link-underline-light text-light">Home</a></p></div>
+  <div class="px-2"><p><a href="#" class="link-underline-light text-light">Event</a></p></div>
+  <div class="px-2"><p><a href="#" class="link-underline-light text-light">Festival</a></p></div>
+ 
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card border border-0 bg-transparent">
+      <div class="card-body">
+      <div class="d-flex flex-row justify-content-around">
+  <div class="px-2"> <p><a href="#" class="link-underline-light text-light">About</a></p></div>
+  <div class="px-2"> <p><a href="#" class="link-underline-light text-light">Blog</a></p></div>
+  <div class="px-2"> <p><a href="#" class="link-underline-light text-light">Contact Us</a></p></div>
+
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+
+  
+  </div>
+<hr class="border border-light border-1 "></hr>
+</div>
+</div>
+    </div>)
+}
+export default Footer;
